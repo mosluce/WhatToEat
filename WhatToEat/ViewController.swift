5f27f3e7c787534d10fb9c41c348535b1c3f07f0
默司 <mosluce@gmail.com>
2016-11-10 09:51:22 +0800

    Initial Commit

---
+++ WhatToEat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WhatToEat
+//
+//  Created by 默司 on 2016/11/10.
+//  Copyright © 2016年 默司. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
